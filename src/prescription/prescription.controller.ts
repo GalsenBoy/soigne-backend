d2602188364fb5d72a51c8f0ab71c08c0784f568
bilaly <billycsk@gmail.com>
2024-04-10 17:14:11 +0200

    prescription avis + ajout dans la bdd

--- src/prescription/prescription.controller.ts
+++ src/prescription/prescription.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { PrescriptionService } from './prescription.service';
+// import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
 
 @Controller('prescription')
-export class PrescriptionController {}
+export class PrescriptionController {
+    constructor(private readonly prescriptionService: PrescriptionService) { }
+
+    // @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    async getPrescriptionsById(id: string) {
+        return this.prescriptionService.getPrescriptionsById(id);
+    }
+}
