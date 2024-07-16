014a2dea7c8a4a4c3712eead9c43bd1506520463
Jokebox90 <dev.jvagnier@gmail.com>
2024-07-16 18:19:52 +0200

    Intègre dotenv dans le processus de démarrage du projet

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { RolesGuard } from './roles/role.guard';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  // Chargement des fichiers '.env'.
+  dotenv.config();
+
+  // Chargement de l'application
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
